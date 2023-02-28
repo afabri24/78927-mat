1a8443a333e067554e05254fd9e82d181e7519f0
afabri24 <afabri24@gmail.com>
2023-02-28 11:13:38 -0600

    tarea 1

--- practica02/Services/Operaciones.cs
+++ practica02/Services/Operaciones.cs
@@ -5,15 +5,17 @@ namespace WSDL.operaciones
 {
     public class Operaciones : Mensajes
     {
+        List<string> saludos = new List<string>();
         public string Saludar(string nombre)
         {
             string msj= "Hola " + nombre;
+            saludos.Add(msj);
             return msj;
         }
 
         public string Mostrar(int id)
         {
-            return "x";
+            return saludos[id+1];
         }
     }
 }
